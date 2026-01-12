18fca07514d2dfede6e40ae348c8ca2a1c409a6f
kalisa <mutabazikalisa9@gmail.com>
2026-01-12 10:53:27 +0200

    Update product context state management

--- src/hooks/contexts/ProductContext.jsx
+++ src/hooks/contexts/ProductContext.jsx
@@ -2,4 +2,25 @@ import { useState, useContext, createContext } from "react";
 
 const ProductContext = createContext();
 
+export const useProduct = () => useContext(ProductContext);
 
+export const ProductProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const value = {
+        products,
+        setProducts,
+        loading,
+        setLoading,
+        error,
+        setError,
+    };
+
+    return (
+        <ProductContext.Provider value={value}>
+            {children}
+        </ProductContext.Provider>
+    );
+};
